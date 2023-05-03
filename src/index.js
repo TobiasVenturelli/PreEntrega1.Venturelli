9b29efd201aafa8c8872772f4581c8f2194538f0
TobiasVenturelli <venturelliquimey@gmail.com>
2023-05-02 21:45:02 -0300

    Agregamos wedsocket y handlebars

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import express from "express";
 import ProductRouter from "./router/product.routes.js"
 import CartRouter from "./router/carts.routes.js"
 
+const handlebars = require('handlebars');
+
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+
+const http = require('http');
+const server = http.Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+});
+
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/products", ProductRouter)
 app.use("/api/cart", CartRouter)
--- src/router/carts.routes.js
+++ src/router/carts.routes.js
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import CartManager from "../controllers/CartManager.js";
+const io = require('socket.io')();
+const router = new Router();
 
 const CartRouter = Router();
 const carts = new CartManager
 
 CartRouter.post("/", async (req, res) => {
     res.send(await carts.addCarts())
+    res.render('home', { products });
 })
+
+CartRouter.get('/realtimeproducts', (req, res) => {
+    res.render('realTimeProducts', { products });
+});
 
 CartRouter.get("/", async (req, res) => {
     res.send(await carts.readCarts())
@@ -22,5 +29,15 @@ CartRouter.post('/:cid/products/:pid', async (req, res) => {
     let productId = req.params.pid
     res.send(await carts.addProductInCart(cartId, productId));
 })
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+
+    socket.on('productAdded', (product) => {
+        addProducts(product);
+        io.emit('updateProducts', product);
+    });
+});
+
 
 export default CartRouter
